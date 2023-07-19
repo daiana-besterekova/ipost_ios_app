31cf7e9ab40d459503cfbb68dc36b2565d2bea07
daiana-besterekova <85840329+daiana-besterekova@users.noreply.github.com>
2023-07-20 03:07:17 +0600

    added styling

--- backend/app.py
+++ backend/app.py
@@ -7,6 +7,7 @@ from color import *
 class ImageDescription(BaseModel):
     description: str
     color: str
+    style: str
 
 class Settings(BaseModel):
     OPENAI_API_KEY: str = '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'
@@ -21,19 +22,20 @@ app = FastAPI()
 @app.post("/generate_image")
 async def generate_image(desc: ImageDescription):
     response = openai.Image.create( 
-        prompt=generate_prompt(desc.description, desc.color),
+        prompt=generate_prompt(desc.description, desc.color, desc.style),
         n=1,
         size="256x256"
     )
     result = response['data'][0]['url']
     return {"url": result}
 
-def generate_prompt(description, color):
+def generate_prompt(description, color, style):
     color = color if color else "rgb(255,255,255)" # default color is white
     color = closest_remove(color)
     print(color)
     text = description if description else "some default prompt"
-    return f"Generate an image of a {text}  and the environment color should be matched with this {color} color"
+    print(f"Generate an image of a {text} in {style} style and the environment color should be matched with this {color} color")
+    return f"Generate an image of a {text} in {style} style and the environment color should be matched with this {color} color"
 
 if __name__ == "__main__":
     uvicorn.run('app:app', host="localhost", port=5001, reload=True)
